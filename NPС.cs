04f7d3f4c5c637df915bd485d458df8c43c63462
Sofa1405 <lsofil.1405@gmail.com>
2024-11-16 11:49:05 +0300

    Add files via upload

---
+++ NPС.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPС : MonoBehaviour
+{
+    int health = 5;
+    int level = 1;
+    float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print("Здоровье игрока " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z +=speed * Time.deltaTime;
+        transform.position = newPosition;
+
+    }
+}
